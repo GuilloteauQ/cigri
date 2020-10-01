7c9ec028ac36051c1e366c9422e7646f3a0132c0
Quentin Guilloteau <Quentin.Guilloteau@inria.fr>
2020-10-01 16:59:05 +0200

    Add cigri-control file
    This aims at making it easier to manage the code of the different
    controllers.
    When developing a new controller, we would only need to implement a
    controller object with the same API as commited here.
    The code in the runner will thus be intact

---
+++ lib/cigri-control.rb
@@ -0,0 +1,55 @@
+require 'cigri-clusterlib'
+
+class Controller
+  # We try to give a generic API for the controller
+  # Here parameters contains most likely a list of all the parameters to use
+  # in the case of a PI controller is will contain two values: the coef Kp and Ki
+  # It is possible to add others attributes:
+  #   - the cumulated error
+  #   - some constant matrices
+  def initialize(logfile, cluster)
+    @nb_jobs = 0
+	@reference = 10 # TODO To set accordingly
+	@error = 0
+	@logfile = logfile # TODO: May need to create the file if does not exist
+	@cluster = cluster
+  end
+
+  def update_controlled_value()
+	# TODO to set accordingly
+	@nb_jobs = 1 * @error
+  end
+
+  def update_error(value)
+	@error = @reference - value
+  end
+
+  def log()
+	file = File.open(@logfile, "a+")
+    file << "#{Time.now.to_i}, #{@nb_jobs}\n"
+    file.close
+  end
+
+  def get_running_jobs()
+	cluster_jobs = @cluster.get_jobs()
+	cluster_jobs.select{j| j["state"] == "Running" or j["state"] == "Finishing" or j["state"] == "Launching"}
+  end
+
+  def get_waiting_jobs()
+	cluster_jobs = @cluster.get_jobs()
+	cluster_jobs.select{j| j["state"] == "Waiting"}
+  end
+
+  def get_cluster_load()
+	@cluster.get_global_stress_factor
+  end
+
+  def get_error()
+	@error
+  end
+
+  def get_nb_jobs()
+	@nb_jobs
+  end
+
+end
