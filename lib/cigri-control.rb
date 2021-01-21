095433e6dad74c85d6324564c0c8b3610afb4b50
Quentin Guilloteau <qguilloteau@frontend-buster>
2021-01-21 09:04:52 +0100

    Scanning: do not start new perf measures if the system does not vary enough

--- lib/cigri-control.rb
+++ lib/cigri-control.rb
@@ -12,18 +12,20 @@ class Controller
     config_data = JSON.parse(File.read(config_file))
     @nb_jobs = config_data["nb_jobs"].nil? ? 0 : config_data["nb_jobs"].to_i
     @reference = 3
+    @threshold = 1
     @error = 0
+
     @logfile = logfile # TODO: May need to create the file if does not exist
     @cluster = cluster
+
     @slices = [0, 5, 10, 15, 20]
-    @iteration = 0
     @perf_slices = [0, 0, 0, 0, 0]
+
     @is_champion_running = false
     @champion = -1
-    @champion_iteration = 0
-    @max_champion_iterations = 6
+    @done_scanning = false
+    @iteration = 0
   end
-
 
   def get_fileserver_load()
     loadavg_per_sensor = []
@@ -69,38 +71,83 @@ class Controller
   end
 
   def get_nb_jobs()
-    print("iteration: #{@iteration}\nis champion running: #{@is_champion_running}\nchampion: #{@champion}\nchampion iter: #{@champion_iteration}\n")
-    @is_champion_running = @is_champion_running && @champion_iteration < @max_champion_iterations
-    if @is_champion_running then
-      @champion_iteration = @champion_iteration + 1
+    # Look if we need to scan
+    if @done_scanning && (@reference - self.get_fileserver_load()).abs > @threshold then
+      @need_to_scan = true
+      @done_scanning = false
+      @is_champion_running = false
+    end
+
+    if @is_champion_running && not @need_to_scan then
       return @slices[@champion]
-    elsif @iteration == @slices.length then
+    end
+
+    if @need_to_scan && @done_scanning then
+      # We look at all the perfs form all the slices
+      # and chose the champion
       @perf_slices[@iteration - 1] = self.get_perf()
       index = 0
       max = @perf_slices[index]
-      for i in 1..@slices.length - 1 do
-        if max[0] < @perf_slices[i][0] then
+      for i in 1..@slices.length do
+        if max[0] < @perf_slices[i][0] then # only look the load part
           index = i
           max = @perf_slices[i]
-        elsif max[0] == @perf_slices[i][0] and max[1] < @perf_slices[i][1]
+        elsif max[0] == @perf_slices[i][0] and max[1] < @perf_slices[i][1] # if (somehow) they have the same load, take the one that yields the most number of jobs
           index = i
           max = @perf_slices[i]
         end
       end
       @champion = index
-      @iteration = 0
-      @champion_iteration = 0
       @is_champion_running = true
+      @need_to_scan = false # TODO:  vraiment ou pas ?
       return @slices[@champion]
-    else
-      @champion = -1
+    end
+
+    if @need_to_scan && not @done_scanning then
       if @iteration != 0 then
-        # not the first iter
         @perf_slices[@iteration - 1] = self.get_perf()
       end
       @iteration = @iteration + 1
+      if @iteration == @slices.length then
+        @done_scanning = true
+      end
       return @slices[@iteration - 1]
     end
   end
+
+  # def get_nb_jobs()
+  #   print("iteration: #{@iteration}\nis champion running: #{@is_champion_running}\nchampion: #{@champion}\nchampion iter: #{@champion_iteration}\n")
+  #   @is_champion_running = @is_champion_running && @champion_iteration < @max_champion_iterations
+  #   if @is_champion_running then
+  #     @champion_iteration = @champion_iteration + 1
+  #     return @slices[@champion]
+  #   elsif @iteration == @slices.length then
+  #     @perf_slices[@iteration - 1] = self.get_perf()
+  #     index = 0
+  #     max = @perf_slices[index]
+  #     for i in 1..@slices.length - 1 do
+  #       if max[0] < @perf_slices[i][0] then
+  #         index = i
+  #         max = @perf_slices[i]
+  #       elsif max[0] == @perf_slices[i][0] and max[1] < @perf_slices[i][1]
+  #         index = i
+  #         max = @perf_slices[i]
+  #       end
+  #     end
+  #     @champion = index
+  #     @iteration = 0
+  #     @champion_iteration = 0
+  #     @is_champion_running = true
+  #     return @slices[@champion]
+  #   else
+  #     @champion = -1
+  #     if @iteration != 0 then
+  #       # not the first iter
+  #       @perf_slices[@iteration - 1] = self.get_perf()
+  #     end
+  #     @iteration = @iteration + 1
+  #     return @slices[@iteration - 1]
+  #   end
+  # end
 
 end
